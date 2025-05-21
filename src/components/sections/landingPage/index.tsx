403ef524d6dc5bc8e1335f6e427f11bb33371b94
ciyek <aripurnomo606@gmail.com>
2025-05-21 15:27:52 +0700

    feat: enhance Navbar with scroll effect and add Card components for services and experiences

--- src/components/sections/landingPage/index.tsx
+++ src/components/sections/landingPage/index.tsx
@@ -4,11 +4,11 @@ import { OurServiceSection } from "./OurServiceSection";
 
 const LandingPage = () => {
   return (
-    <>
+    <div className="flex flex-col container mx-auto">
       <HeroSection />
       <OurServiceSection />
       <ExperienceSection />
-    </>
+    </div>
   );
 };
 
