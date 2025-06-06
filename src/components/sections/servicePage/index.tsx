5e1603f70a1243b11d6e6be5f6d58ab4f5de8a5f
ciyek <aripurnomo606@gmail.com>
2025-06-06 21:16:12 +0700

    feat: Enhance ServiceSection with GSAP animations and RevealOnScroll for improved visual effects

--- src/components/sections/servicePage/index.tsx
+++ src/components/sections/servicePage/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Footer } from "../../parts/Footer";
 import { HeadTitle } from "@/components/parts/HeadTitle";
 import { ServiceSection } from "./serviceSection";
+import { RevealOnScroll } from "@/animations/RevealOnScroll";
 
 const ServicePageSection = () => {
   return (
     <div className="flex flex-col container mx-auto">
-      <HeadTitle
-        title="Our Services"
-        description="Discover a portfolio of visually stunning and strategically crafted digital projects that showcase our creativity and expertise."
-      />
+      <RevealOnScroll from={{ opacity: 0, y: -100 }}>
+        <HeadTitle
+          title="Our Services"
+          description="Discover a portfolio of visually stunning and strategically crafted digital projects that showcase our creativity and expertise."
+        />
+      </RevealOnScroll>
       <ServiceSection />
       <Footer />
     </div>
