e1fd33128e0f1b57c590b73e8e02d7e6e99e5ac4
ciyek <aripurnomo606@gmail.com>
2025-06-07 21:49:56 +0700

    feat: Update Login and Register sections with improved styling and additional buttons

--- src/components/sections/registerSection/index.tsx
+++ src/components/sections/registerSection/index.tsx
@@ -7,21 +7,35 @@ const RegisterSection = () => {
     <div className="flex items-center justify-center min-h-screen">
       <div style={{ width: "100%", height: "600px", position: "relative" }}>
         <Threads amplitude={1} distance={0} enableMouseInteraction={true} />
-        <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white/20 backdrop-blur-xs w-[800px] h-auto rounded-[24px] shadow-2xl z-10 px-10 pb-10">
-          <h1 className="text-3xl text-center py-4">Register</h1>
+        <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#262626]/20 backdrop-blur-xs w-[800px] h-auto rounded-[24px] shadow-2xl z-10 px-10 pb-10">
+          <h1 className="text-3xl text-center py-4 text-[#9EFF00]">Sign Up</h1>
           <div className="flex flex-col gap-4 py-4">
             <label>Email</label>
-            <Input type="email" placeholder="Email" />
+            <Input
+              type="text"
+              className="border-0 border-b-2 rounded-none px-0"
+              placeholder="Your Email"
+            />
           </div>
           <div className="flex flex-col gap-4 py-4">
             <label>Password</label>
-            <Input type="password" placeholder="Password" />
+            <Input
+              type="text"
+              className="border-0 border-b-2 rounded-none px-0"
+              placeholder="Your Password"
+            />
           </div>
           <div className="flex flex-col gap-4 py-4">
             <label>Confirm Password</label>
-            <Input type="password" placeholder="Confirm Password" />
+            <Input
+              type="text"
+              className="border-0 border-b-2 rounded-none px-0"
+              placeholder="Your Confirm Password"
+            />
           </div>
-          <Button className="w-full mt-4">Register</Button>
+          <Button variant="primary" className="w-full mt-4">
+            Register
+          </Button>
         </form>
       </div>
     </div>
