3aaab5d4385e472cba6b082262183ae48a4f54ab
ciyek <aripurnomo606@gmail.com>
2025-06-08 12:12:19 +0700

    feat: Add About page metadata and sitemap generation for improved SEO

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,29 @@
-import AboutPageSection from "@/components/sections/aboutPage";
 import React from "react";
+import AboutPageSection from "@/components/sections/aboutPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description:
+    "Welcome to summerville, where collaboration, expertise, and client-centricity intersect to shape the future of digital innovation.",
+  openGraph: {
+    title: "About Us",
+    description:
+      "Welcome to summerville, where collaboration, expertise, and client-centricity intersect to shape the future of digital innovation.",
+    url: "https://summervilleintech.vercel.app/about",
+    siteName: "summerville",
+    images: [
+      {
+        url: "../../assets/images/summerville2.png",
+        width: 1200,
+        height: 630,
+        alt: "Summerville Agency",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 const AboutPage = () => {
   return (
